d5b54cea22c47c60c2a7742ae3c12c0510693391
lcolsant <lee.colsant@gmail.com>
2019-10-30 15:06:16 -0700

    deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'css', 'style.css')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
